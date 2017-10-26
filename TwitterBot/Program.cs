2e8b00a17b9ee46ad08b458e7d19ddc171ffaf04
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-10-26 21:49:31 +0200

    just put in tokens

--- TwitterBot/Program.cs
+++ TwitterBot/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using TweetSharp;
 
 namespace TwitterBot
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        //handle tokens
+        private static readonly string customer_Key = "KEY";
+        private static readonly string customer_key_Sercret = "KEY";
+        private static readonly string access_Token = "KEY";
+        private static readonly string acess_Token_Sercret = "KEY";
+
+        private static readonly TwitterService Service = new TwitterService(customer_Key, customer_key_Sercret, access_Token, acess_Token_Sercret);
+
+
+        private static void Main(string[] args)
         {
+            //sender twitter
+            SendTweet("Hello world");
+        }
+
+
+        //send at tweet
+        private static void SendTweet(string _data)
+        {
+            Service.SendTweet(new SendTweetOptions {Status = _data}, (tweet, response) =>
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                    Console.WriteLine("Tweet sent");
+                else
+                    Console.WriteLine("Fejl! see tokens ok");
+            });
         }
     }
 }
